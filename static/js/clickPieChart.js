292e104f7f77c3eea1523d48f6ec256ac6f23684
jasonhan3 <98669315+jasonhan3@users.noreply.github.com>
2023-09-23 13:33:49 -0500

    Tried wheel

--- static/js/clickPieChart.js
+++ static/js/clickPieChart.js
@@ -1,6 +1,47 @@
 // script.js
 
 document.addEventListener('DOMContentLoaded', function () {
+
+    // $( function() {
+    //     $("#spinner").spinner();
+    // } );
+
+    const wheel = document.getElementById('wheel');
+    let selectedNumber = 0;
+
+    wheel.addEventListener('wheel', (event) => {
+        // Determine the direction of the scroll
+        var delta = event.deltaY;
+
+        console.log("Delta base:", delta);
+
+        const delta_sign = Math.sign(event.deltaY);
+
+        delta = Math.min(Math.abs(delta / 4), 4);
+
+        delta = delta * delta_sign;
+
+        console.log("Delta:", delta);
+
+        // const dampingFactor = 2.4;
+        // const adjustedDelta = delta / dampingFactor;
+        
+        // Update the selected number
+        selectedNumber += delta;
+        console.log("selectedNumber:", selectedNumber);
+        selectedNumber = Math.round(selectedNumber);
+        console.log("selectedNumber rounded:", selectedNumber);
+        if (selectedNumber < 0) selectedNumber = 9;
+        if (selectedNumber > 9) selectedNumber = 0;
+        
+        // Update the wheel position
+        wheel.style.transform = `translateY(-${selectedNumber * 50}px)`;
+        
+        // Prevent the default scroll behavior
+        event.preventDefault();
+    });
+
+    
     // Get a reference to the canvas element
     var ctx = document.getElementById('myPieChart').getContext('2d');
 
@@ -32,14 +73,21 @@ document.addEventListener('DOMContentLoaded', function () {
         if (activeElements.length > 0) {
             // Get the index of the clicked segment
             var clickedIndex = activeElements[0].index;
+            
+            console.log(myPieChart.getDatasetMeta(0).data[clickedIndex]);
+            console.log(myPieChart.chartArea);
+            const angle = myPieChart.getDatasetMeta(0).data[clickedIndex].startAngle + (myPieChart.getDatasetMeta(0).data[clickedIndex].circumference / 2);
+            const radius = myPieChart.getDatasetMeta(0).data[clickedIndex].outerRadius;
+            const x = radius * Math.cos(angle) + myPieChart.chartArea.left + radius;
+            const y = radius * Math.sin(angle) + myPieChart.chartArea.top + radius;
 
             // myPieChart.data.datasets[0].data[clickedIndex] += 10
 
             var popup = document.getElementById('popup');
             
             // Position the popup next to the clicked segment
-            popup.style.left = event.clientX / 2 + 'px';
-            popup.style.top = event.clientY / 2 + 'px';
+            popup.style.left = x + 'px';
+            popup.style.top = y + 'px';
             popup.style.display = 'block';
 
             // Handle the "Update" button click
