c2af9b1b3be7fccf737dbe13206a36e81e61e81b
David Zhu <dsz1@rice.edu>
2023-09-23 13:37:45 -0500

    User init and ChatGPT interaction v1

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
+from flask_socketio import SocketIO
 import user
 
 # Create a Flask app
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 
 # Define a route and a view function
@@ -10,16 +12,49 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Flask!'
 
+# Sending push data notifications to front end indicating that server data has changed
+# and the front-end displays need to be updated with this new information.
+@socketio.on('connect')
+def handle_connect():
+    print("fe connected")
+    socketio.emit('response', {'data': 'connected'})
+
 
 # Run the app if this script is executed
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
 
+@app.route('/api/inituser', methods=['POST'])
+def init_user():
+    '''
+    Creates a User object to store requisite fields and data.
+    So far, only requires monthly income and something else. 
+    '''
+    userzip = request.get_json()["zip"]
+    userinc = request.get_json()["income"]
+    userobj = user.User(userzip, float(userinc))
 
-@app.route('/api/initialconfig', methods=['POST'])
-def get_init_config():
-    return
+    pair = userobj.median_rent_utilities()
 
+    overall = {"rent": pair[0],
+                "savings": 0,
+                "utilities": pair[1], 
+                "necessities" : 500,
+                "discretionary": float(userinc) - pair[0] - pair[1] - 500}
+    utilities = {"electricity": float(pair[1])/3,
+                 "water": float(pair[1])/3,
+                 "gas": float(pair[1])/3}
+    necessities = {"food": 500, 
+                   "transportation": 500, 
+                   "misc": 0 }
+    discretionary = {"misc": overall["discretionary"]}
+
+    userobj.piecharts = {"overall":overall,
+                         "utilities":utilities,
+                         "necessities":necessities,
+                         "discretionary":discretionary}
+
+    return jsonify(userobj.piecharts)
 
 @app.route('/api/userrequest', methods=['POST'])
 def fwd_req():
@@ -27,6 +62,9 @@ def fwd_req():
     # It maps "request" to the full list of user requests.
     # Which are each
     chat_hist = request.get_json()["request"]
+
     recent_req = chat_hist[-1]["content"]
     print(recent_req)
     return "sup"
+
+
--- censusapi.py
+++ censusapi.py
@@ -3,7 +3,6 @@ from census import Census
 API_KEY = "32e797a32ba6e8830a02ac4a88804ec08124e470"
 c = Census(API_KEY)
 
-
 def get_med_inc_nonfamily(zipcode):
     """
     Returns the median income in a given zip code for
@@ -19,16 +18,15 @@ def get_med_inc_nonfamily(zipcode):
     table_code = 'B19202_001E'
 
     # Return value is a list of dictionary.
-    api_return = c.acs5.zipcode(table_code, zipcode, state_fips=0)
+    api_return = c.acs5.zipcode((table_code), zipcode, state_fips = 0)
 
     # If we have an invalid entry, give average for US.
-    if len(api_return) == 0:
-        api_return = c.acs5.us(table_code)
-
+    if (len(api_return) == 0):
+        api_return = c.acs5.us((table_code))
+    
     # Unwrap the api value 
     ret = api_return[0][table_code]
     return ret
-
 
 def get_med_inc_family(zipcode):
     """
@@ -40,21 +38,20 @@ def get_med_inc_family(zipcode):
         - An integer representing the median income for households.
         - -1 if input invalid or other error occurred
     """
-
+    
     # The table lookup corresponding to the data that we want.
     table_code = 'B19013_001E'
 
     # Return value is a list of dictionary.
-    api_return = c.acs5.zipcode(table_code, zipcode, state_fips=0)
+    api_return = c.acs5.zipcode((table_code), zipcode, state_fips = 0)
 
     # If we have an invalid zipcode, give average for US.
-    if len(api_return) == 0:
-        api_return = c.acs5.us(table_code)
-
+    if (len(api_return) == 0):
+        api_return = c.acs5.us((table_code))
+    
     # Unwrap the api value 
     ret = api_return[0][table_code]
     return ret
-
 
 def get_med_rent(zipcode):
     """
@@ -65,17 +62,41 @@ def get_med_rent(zipcode):
         - An integer representing the median rent.
         - -1 if input invalid or other error occurred
     """
-
+    
     # The table lookup corresponding to the data that we want.
     table_code = 'B25058_001E'
 
     # Return value is a list of dictionary.
-    api_return = c.acs5.zipcode(table_code, zipcode, state_fips=0)
+    api_return = c.acs5.zipcode((table_code), zipcode, state_fips = 0)
 
     # If we have an invalid zipcode, give average for US.
-    if len(api_return) == 0:
-        api_return = c.acs5.us(table_code)
+    if (len(api_return) == 0):
+        api_return = c.acs5.us((table_code))
+    
+    # Unwrap the api value 
+    ret = api_return[0][table_code]
+    return ret
 
+def get_med_housing_cost(zipcode):
+    """
+    Returns the median housing cost in a given zipcode.
+    Requires:
+        - zipcode to be the string form of a valid zipcode.
+    Returns: 
+        - An integer representing the median housing cost.
+        - -1 if input invalid or other error occurred
+    """
+    
+    # The table lookup corresponding to the data that we want.
+    table_code = 'B25105_001E'
+
+    # Return value is a list of dictionary.
+    api_return = c.acs5.zipcode((table_code), zipcode, state_fips = 0)
+
+    # If we have an invalid zipcode, give average for US.
+    if (len(api_return) == 0):
+        api_return = c.acs5.us((table_code))
+    
     # Unwrap the api value 
     ret = api_return[0][table_code]
     return ret
