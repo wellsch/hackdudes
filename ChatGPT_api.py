072a13e54b91001a89d490938fc8ce4506885e77
David Zhu <dsz1@rice.edu>
2023-09-23 10:49:53 -0500

    framework for ChatGPT API

--- ChatGPT_api.py
+++ ChatGPT_api.py
@@ -1,9 +1,15 @@
 import os
 import openai
-
-ChatGPT_API_KEY = "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"
+# Make your own config file with the OpenAI API Key
+import config
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv(ChatGPT_API_KEY)
+openai.api_key = config.ChatGPT_API_KEY
 
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello world"}])
+chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
+                                               messages=[{"role": "user", "content": "I am a 20 year old male living in Brookline MA."},
+                                                         {"role": "user", "content": "I pay 1000$ a month in rent."}, 
+                                                         {"role": "user", "content": "I make 20$ a hour at McDonalds and work 40 hours a week"}, 
+                                                         {"role": "user", "content": "Give me ONLY a JSON object of my zip code, monthly income, age, and rent."} 
+                                                         ])
+print(chat_completion)
